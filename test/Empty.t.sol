8255eda79ef7303aed10d8e8f2fda73b588f6351
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-10 10:56:40 -0400

    updated

--- test/Empty.t.sol
+++ test/Empty.t.sol
@@ -7,6 +7,11 @@ import "../src/Empty.sol";
 contract EmptyTest_Unit is Test {
     function setUp() public {}
 
+    // Setup
+    uint256 constant FORK_BLOCK_NUMBER = 17149178; // All tests executed at this block
+    string RPC_MAINNET = "RPC_MAINNET";
+    uint256 forkId = vm.createSelectFork(vm.envString(RPC_MAINNET), FORK_BLOCK_NUMBER);
+
     function testTest() public {
         Empty empty = new Empty();
     }
